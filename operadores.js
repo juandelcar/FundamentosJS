a846eeaa97fd87077f051c9d7dbe698b7b2cca44
juandelcar <juandelcar123c@gmail.com>
2023-09-08 14:54:54 -0600

    03 Operadores

---
+++ operadores.js
@@ -0,0 +1,35 @@
+// Operadores Matematicos
+
+console.log(1+1)
+console.log(1-1)
+console.log(1*1)
+console.log(1/1)
+console.log(1%1)
+console.log(3**3)
+
+// mas operadores
+let a = 10
+a += 10 // a = a + 10 
+console.log(a)
+a -= 10 // a = a - 10 
+console.log(a)
+a *= 10 // a = a * 10 
+console.log(a)
+a /= 10 // a = a / 10 
+console.log(a)
+a %= 10 // a = a % 10 
+console.log(a)
+console.log(a)
+
+// Operadores Relacionales
+console.log(10 < 10)
+console.log(10 <= 10)
+console.log(10 > 10)
+console.log(10 >= 10)
+console.log(10 !== 10)
+console.log(10 === 10)
+
+// Operadores Logicos
+console.log(true && true)
+console.log(true || true)
+console.log(!true)
