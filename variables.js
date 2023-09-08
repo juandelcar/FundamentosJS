490b2e45a2a381952d33a71473fb2b418effed9e
juandelcar <juandelcar123c@gmail.com>
2023-09-08 14:40:12 -0600

    02 Variables let y const

---
+++ variables.js
@@ -0,0 +1,32 @@
+//  JS no es tipado por lo que n0 se especifica tipo de dato
+
+// Para declaracion de variables vamos a estar utilizando const y let
+// const para variables de solo lectura
+//let para variables
+
+const pi = 3.14
+// No podremos asiganr nuevos valores a const
+// pi = "tres"
+
+// debrmos inicializar siempre los const
+// const g
+// g = 1.89
+
+let conatdor
+console.log(conatdor)
+
+conatdor=10
+conatdor +=1
+conatdor +=1
+conatdor +=1
+conatdor +=1
+
+console.log(conatdor)
+
+conatdor = "Ahora soy un string"
+console.log(conatdor)
+
+let verdad = true
+console.log(typeof(verdad))
+console.log(typeof(conatdor))
+
