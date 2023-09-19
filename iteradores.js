89044efc44e8c71e22bc8012daefdd17f76cecb8
juandelcar <juandelcar123c@gmail.com>
2023-09-19 12:50:30 -0600

    11 Iteradores

---
+++ iteradores.js
@@ -0,0 +1,73 @@
+// Iteradores de Arrays
+const fruits = ['apple', 'melon', 'orange', 'cherry'];
+
+// forEach
+// Itera array
+fruits.forEach(
+    function (fruit){
+        console.log(`Fruta ${ fruit }`)
+    } 
+);
+
+console.log('Arrow Function')
+fruits.forEach(
+     (fruit) => {
+        console.log(`Fruta ${ fruit }`)
+    } 
+);
+
+console.log('Arrow Function Simplified')
+fruits.forEach(
+     fruit => console.log(`Fruta ${ fruit }`)     
+);
+
+// map()
+// Regresa un array nuevo a partir del original
+const fruitMayus = fruits.map(
+    fruit => {
+        return fruit.toUpperCase();
+    }
+);
+console.log('* '.repeat(20));
+console.log(fruits);
+console.log(fruitMayus);
+
+const numbers = [1,2,3,4,5]
+const squares = numbers.map(num => num * num)
+console.log(numbers);
+console.log(squares);
+
+// Filter()
+// crea un array nuevo a partir de una condicion
+console.log('* '.repeat(15) + 'Filter');
+const startsWithA = fruits.filter(fruit => fruit.endsWith('e'));
+console.log(fruits);
+console.log(startsWithA);
+
+const pares = numbers.filter(num => num % 2 !== 0)
+console.log(numbers);
+console.log(pares);
+
+// reduce()
+// Retorna un uncio valor a partir del array original
+const sumatoria = numbers.reduce((a ,b) => a + b);
+console.log(numbers);
+console.log(sumatoria);
+
+const factorial = numbers.reduce((a, b) => a * b);
+console.log(numbers);
+console.log(factorial);
+
+// some()
+// evalua cada elemento retorna true si al menos
+// uno cumple una condicion
+console.log(numbers.some(num => num >= 5));
+console.log(numbers.some(num => num > 6));
+
+// every()
+// regresa true si todos cumplen
+console.log(numbers.every(num => num > 0));
+console.log(numbers.every(num => num > 4));
+
+console.log(fruits);
+console.log(fruits.join(' - '));
